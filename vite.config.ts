1e1e0cf3582b81760a699c427f43c7b609167859
Renato Wagner Anunciação <wagnerpunk002@gmail.com>
2025-05-05 00:08:05 -0300

    fix: ajuste na saida do build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:  '/finances/',
+  base: '/finances/',
+  build: {
+    outDir: 'dist/finances',
+  },
   plugins: [react()],
 })
