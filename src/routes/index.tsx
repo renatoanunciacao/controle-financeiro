3454ab650393f008c19e2886335b652706e44ec0
Renato Wagner Anunciação <wagnerpunk002@gmail.com>
2025-05-05 05:02:40 -0300

    fix: ajustando o roteamento

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navigate, Route, BrowserRouter } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import Login from '../pages/login'
 import Home from '../pages/home'
 import PrivateRoute from '../components/private-route'
@@ -7,33 +7,33 @@ import PublicRoute from '../components/public-route'
 
 const AppRoutes: React.FC = () => {
 
-    const renderRoutes = () => {
-        return (
-            <BrowserRouter>
-            <Route
-              path="/login"
-              element={
-                <PublicRoute>
-                  <Login />
-                </PublicRoute>
-              }
-            />
-      
-            <Route
-              path="/home"
-              element={
-                <PrivateRoute>
-                  <Home />
-                </PrivateRoute>
-              }
-            />
-      
-            <Route path="*" element={<Navigate replace to="/login" />} />
-          </BrowserRouter>
-        )
-    }
+  const renderRoutes = () => {
+    return (
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
 
-    return <>{renderRoutes()}</>
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate replace to="/login" />} />
+      </Routes>
+    )
+  }
+
+  return <>{renderRoutes()}</>
 }
 
 export default AppRoutes;
