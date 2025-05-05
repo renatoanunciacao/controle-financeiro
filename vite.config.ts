8b5b1e808423478922e2e088161668a4fa9f7088
Renato Wagner Anunciação <wagnerpunk002@gmail.com>
2025-05-05 00:18:25 -0300

    fix: correção na configuração de roteamento e no package

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/finances/',
-  build: {
-    outDir: 'dist/finances',
-  },
   plugins: [react()],
 })
