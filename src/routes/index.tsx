23209056a51e0d3d4f65df68325d7bda393db268
Renato Wagner Anunciação <wagnerpunk002@gmail.com>
2025-05-10 01:47:57 -0300

    feat: ajustando as rotas, tentativa 2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,7 +15,7 @@ interface ErrorBoundaryState {
 class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   state: ErrorBoundaryState = { hasError: false };
 
-  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+  static getDerivedStateFromError(): ErrorBoundaryState {
     return { hasError: true };
   }
 
